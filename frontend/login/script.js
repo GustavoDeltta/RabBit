0ed753c0571da61bd9a9a5972b4973c422074cd6
GustavoDeltta <deltta.dev@gmail.com>
2024-11-25 09:13:56 -0300

    Alterações no estilo da pagina de login.

--- frontend/login/script.js
+++ frontend/login/script.js
@@ -1,10 +1,13 @@
 const container = document.querySelector('.container');
 const registerBtn = document.querySelector('.register-btn');
 const loginBtn = document.querySelector('.login-btn');
+var r = document.querySelector(':root')
 
 registerBtn.addEventListener('click', () => {
     container.classList.add('active');
+    r.style.setProperty('--angle-background', '270deg');
 });
 loginBtn.addEventListener('click', () => {
     container.classList.remove('active');
+    r.style.setProperty('--angle-background', '90deg');
 });
